b30d23f9bcea265a536c5ac2d71858e9b79ac352
tsm <441953232@qq.com>
2020-08-31 16:58:05 +0800

    feat: init front-control

---
+++ front-control/src/main/java/com/tsm/feastful/frontcontrol/FrontControlApplication.java
@@ -0,0 +1,13 @@
+package com.tsm.feastful.frontcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontControlApplication.class, args);
+    }
+
+}
---
+++ front-control/src/test/java/com/tsm/feastful/frontcontrol/FrontControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsm.feastful.frontcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
