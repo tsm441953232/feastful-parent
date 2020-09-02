b0deb0f1e178f5d698bffb29dc901dd34b042aef
tsm <441953232@qq.com>
2020-09-02 16:27:51 +0800

    feat: init front-control

---
+++ front-control/src/sql/db_init_sql/V1_1_1__fc_account.sql
@@ -0,0 +1,20 @@
+-- ----------------------------
+-- Table structure for fc_account
+-- ----------------------------
+CREATE TABLE fc_account (
+  id bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '物理主键',
+  cust_id varchar(64) COMMENT '客户号',
+  phone_num varchar(16) COMMENT '手机号码',
+  business_type varchar(64) NOT NULL COMMENT '业务类型',
+  acc_password varchar(48) COMMENT '密码',
+  acc_status tinyint(4) COMMENT '0-正常,1-冻结',
+  date_invalid datetime COMMENT '冻结时间',
+  created_by varchar(32) COMMENT '创建者',
+  date_create datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  updated_by varchar(32) COMMENT '更新人',
+  date_update datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  del_flag tinyint(1) NOT NULL DEFAULT '0' COMMENT '逻辑删除',
+  PRIMARY KEY (id),
+  UNIQUE KEY uniq_key_1 (phone_num)
+) COMMENT='用户表';
+
