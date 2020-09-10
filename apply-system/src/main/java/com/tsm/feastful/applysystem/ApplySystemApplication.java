a32c509626c35dc2d80dbb8e48436f7594e3c4f9
tsm <441953232@qq.com>
2020-09-10 15:43:13 +0800

    feat: init aps

---
+++ apply-system/src/main/java/com/tsm/feastful/applysystem/ApplySystemApplication.java
@@ -0,0 +1,13 @@
+package com.tsm.feastful.applysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplySystemApplication.class, args);
+    }
+
+}
---
+++ apply-system/src/test/java/com/tsm/feastful/applysystem/ApplySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsm.feastful.applysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
